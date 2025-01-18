9fb33c2df8ea2563cc8bfcef9cd2e091ba86cceb
twndus <twndus1362@gmail.com>
2025-01-17 23:15:50 -0500

    fix: sungkim lab 03 fixed

--- sungkim/03_dummy_q_learning.py
+++ sungkim/03_dummy_q_learning.py
@@ -1,51 +1,51 @@
 # inspired by lab 3
+from tqdm import tqdm
+
 import numpy as np
+import matplotlib.pyplot as plt
 import gymnasium as gym
 
 def main():
     # create environment
-    env = gym.make('FrozenLake-v1', render_mode="human", is_slippery=False)
-
-    # reset and observe current state
-    state, _ = env.reset()
-
-#    # print action_space
-#    print(env.action_space.n)
-#
-#    # print observation_space
-#    print(env.observation_space)
+    env = gym.make('FrozenLake-v1', is_slippery=False)#, render_mode="human", is_slippery=False)
 
     # Q table (obs, actions: left, down, right, up)
-    qtable = np.zeros((env.observation_space.n, env.action_space.n))
-    print((env.observation_space.n, env.action_space.n))
+    Q = np.zeros((env.observation_space.n, env.action_space.n))
 
     # define argmax function by policy: greedy but random for ties
-    def randargmax(state: int):
-        return np.random.choice(np.where(qtable[state,:] == qtable[state,:].max())[0])
+    def randargmax(vector: np.array):
+        return np.random.choice(np.where(vector == vector.max())[0])
 
-    # execute episode
-    for _ in range(1000):
-        # env.render()
+    # execute episodes
+    num_episodes = 2000
+    reward_list = []
+
+    for i in tqdm(range(num_episodes)):
+
+        # reset and observe current state
+        state, _ = env.reset()
+        done = False
         
-#        # choose actions randomly
-#        action = env.action_space.sample()
+        while not done:
+            # choose actions by q table
+            action = randargmax(Q[state,:])
 
-        # choose actions by q table
-        action = randargmax(state)
+            # transition to next state
+            next_state, reward, done, _, _ = env.step(action)
 
-        # transition to next state
-        next_state, reward, done, _, _ = env.step(action)
+            # update Q 
+            Q[state, action] = reward + np.max(Q[next_state, :])
 
-        # update qtable
-        qtable[state, action] = reward + np.max(qtable[next_state, :])
+            # transit to next state
+            state = next_state
+            
+        reward_list.append(reward)
 
-        # transit to next state
-        state = next_state
+    print(f'success rate: {sum(reward_list)/num_episodes*100}')
+    print(f'Q table: {Q}')
 
-        if done:
-            state, _ = env.reset()
-
-    print(qtable)
+    plt.bar(range(num_episodes), reward_list) 
+    plt.show()
 
 if __name__ == '__main__':
     main()
